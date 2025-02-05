a398d2e5e665392e8e5ff97bef0281277520d098
Alexander Smirnov <alexander.smirnov@cariad.technology>
2025-02-05 15:53:05 +0100

    heap: Add buddy allocator
    This commit adds simple buddy heap allocator. Also now we have stack
    reserved in linker script.
    
    Signed-off-by: Alexander Smirnov <alexander.smirnov@cariad.technology>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 aarch64-unknown-linux-gnu-c++ -c entry/boot_entry_point.S
 aarch64-unknown-linux-gnu-c++ -c entry/early_print/uart_pl011.S
 
-cargo build --target aarch64-unknown-linux-gnu --release --manifest-path kernel/Cargo.toml
+cargo build --target aarch64-unknown-linux-gnu --release --manifest-path kernel/Cargo.toml -Zbuild-std
 
 aarch64-unknown-linux-gnu-ld -EL boot_entry_point.o uart_pl011.o kernel/target/aarch64-unknown-linux-gnu/release/libsaturn_kernel.a -T saturn.lds -o saturn.bin
 aarch64-unknown-linux-gnu-objcopy -O binary -S saturn.bin saturn
--- kernel/src/kernel.rs
+++ kernel/src/kernel.rs
@@ -10,12 +10,32 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the 
 // specific language governing permissions and limitations under the License.
 
+
 const PL011_REG_BASE: usize = 0x0900_0000;
 const PL011_REG_DR: usize = 0x00;
 
 static LOGO: &[u8] = b"start kernel...\r\n";
 
+extern crate alloc;
+
+use buddy_system_allocator::LockedHeap;
+use alloc::vec::Vec;
+
+// Saturn heap configuration. We are using buddy allocator, so specify order is enough.
+const HEAP_ORDER: usize = 18;
+const HEAP_SIZE: usize = 1 << HEAP_ORDER;
+#[global_allocator]
+static SATURN_HEAP_ALLOCATOR: LockedHeap<HEAP_ORDER> = LockedHeap::<HEAP_ORDER>::new();
+static SATURN_HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
+
 pub(crate) fn init() -> ! {
+    unsafe {
+        SATURN_HEAP_ALLOCATOR.lock().init(SATURN_HEAP.as_ptr() as usize, SATURN_HEAP.len());
+    }
+
+    let mut vec = Vec::new();
+    vec.push(1);
+
     let dr = (PL011_REG_BASE + PL011_REG_DR) as *mut u8;
 
     for c in LOGO {
